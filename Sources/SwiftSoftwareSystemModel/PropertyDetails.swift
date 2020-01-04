5b336b152b12d719e71e486e44322bb7d0d3878e
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-01-04 17:04:50 -0500

    Provide for flagging something as a function

--- Sources/SwiftSoftwareSystemModel/PropertyDetails.swift
+++ Sources/SwiftSoftwareSystemModel/PropertyDetails.swift
@@ -3,13 +3,16 @@ public struct PropertyDetails {
 
     public let optional: Bool
     public let tuple: Bool
+    public let function: Bool
     
     public init(
         optional: Bool = false,
-        tuple: Bool = false
+        tuple: Bool = false,
+        function: Bool = false
     ) {
         self.optional = optional
         self.tuple = tuple
+        self.function = function
     }
     
 
